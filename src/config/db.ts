6dbcd563662d7577a2447ca2ea9ab98f986fde79
anbya <anbyaalibia@gmail.com>
2024-12-20 20:58:33 +0700

    implement credential encryption

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,12 +1,15 @@
 import { Pool } from 'pg';
 import * as dotenv from 'dotenv';
+import { aes256gcm } from '../authorization/secret';
+
+const cipher = aes256gcm(Buffer.alloc(32));
 dotenv.config();
 
 const pool = new Pool({
-  user: process.env.DB_USERNAME,
-  host: process.env.DB_HOST,
-  database: process.env.DB_DEV_NAME,
-  password: process.env.DB_PASSWORD,
+  user: cipher.decrypt(process.env.DB_USERNAME as string),
+  host: cipher.decrypt(process.env.DB_HOST as string),
+  database: cipher.decrypt(process.env.DB_DEV_NAME as string),
+  password: cipher.decrypt(process.env.DB_PASSWORD as string),
   port: 5432,
 });
 
--- src/config/seqDB.ts
+++ src/config/seqDB.ts
@@ -1,11 +1,14 @@
 import { Sequelize } from 'sequelize-typescript';
 import * as dotenv from 'dotenv';
+import { aes256gcm } from '../authorization/secret';
+
+const cipher = aes256gcm(Buffer.alloc(32));
 dotenv.config();
 
 export const sequelizeConfig = new Sequelize({
   dialect: 'postgres',
-  host: process.env.DB_HOST,
-  username: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DEV_NAME,
+  host: cipher.decrypt(process.env.DB_HOST as string),
+  username: cipher.decrypt(process.env.DB_USERNAME as string),
+  password: cipher.decrypt(process.env.DB_PASSWORD as string),
+  database: cipher.decrypt(process.env.DB_DEV_NAME as string),
 });
